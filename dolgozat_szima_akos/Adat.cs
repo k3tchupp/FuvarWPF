41a4dc0884e1c2c9c76638a8946a70a17bee39de
k3tchupp <70635906+k3tchupp@users.noreply.github.com>
2023-09-19 13:04:44 +0200

    Add files via upload

---
+++ dolgozat_szima_akos/Adat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozat_szima_akos
+{
+    internal class Adat
+    {
+        public int taxi_id;
+        public DateTime indulas;
+        public string idotartam;
+        public int tavolsag;
+        public double vitelidij;
+        public double borravalo;
+        public string fizetes_modja;
+
+        public Adat(string s)
+        {
+            string[] darabok = s.Split(';');
+            this.taxi_id = Convert.ToInt32( darabok[0]);
+            this.indulas = Convert.ToDateTime( darabok[1]);
+            this.idotartam = darabok[2];
+            this.vitelidij = Convert.ToDouble(darabok[3]);
+            this.borravalo = Convert.ToDouble(darabok[4]);
+            this.fizetes_modja = darabok[5];
+        }
+
+        public double MerfolValtas(double merfold)
+        {
+            double tavolsag = merfold * 1.6;
+            return tavolsag;
+        }
+    }
+}
---
+++ dolgozat_szima_akos/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dolgozat_szima_akos
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Adat> lista = new List<Adat>();
+
+            using(StreamReader sr = new StreamReader("fuvar.csv"))
+            {
+                sr.ReadLine();
+                while(!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    Adat s = new Adat(sor);
+                    lista.Add(s);
+                }
+            }
+            // 3.
+            MessageBox.Show(Convert.ToString(lista.Count));
+
+            // 4.
+            Adat azonositoju = lista.Where(x => x.taxi_id == 6185).First();
+
+            double bevetel = azonositoju.vitelidij + azonositoju.borravalo;
+            int fuvarok_szama = lista.Where(x => x.taxi_id == 6185).Count();
+
+            MessageBox.Show(Convert.ToString(bevetel), Convert.ToString(fuvarok_szama) );
+
+            // 5.
+            foreach (var item in lista.GroupBy(x=>x.fizetes_modja))
+            {
+                MessageBox.Show(item.Key, Convert.ToString( item.Count()));
+            }
+
+
+            // 6.
+
+            double osszesen = lista.Sum(x => x.MerfolValtas(x.tavolsag));
+            MessageBox.Show(Convert.ToString(osszesen));
+
+            // 7.
+
+            Adat leghosszabb = lista.OrderBy(x => x.idotartam).First();
+            MessageBox.Show(Convert.ToString(leghosszabb.taxi_id, leghosszabb.indulas, leghosszabb.idotartam, leghosszabb.tavolsag, leghosszabb.vitelidij, leghosszabb.borravalo, leghosszabb.fizetes_modja));
+
+            // 8.
+
+            /*foreach (var item in lista)
+            {
+                if (Convert.ToInt32( item.idotartam) > 0 && item.vitelidij > 0 && item.tavolsag == 0)
+                {
+                    
+                }
+            }*/
+
+            //File.WriteAllText("hibak.txt", )
+
+        }
+    }
+}
